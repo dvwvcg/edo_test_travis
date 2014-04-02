94c5b10b797139dbde3b81b785df1956ac7af835
David vanderWatt <dvanderwatt@vepcg.com>
2014-04-01 23:30:31 -0700

    go to quote

--- test/test_home_page.rb
+++ test/test_home_page.rb
@@ -18,18 +18,23 @@ module Test
     end
 
 
-    def test_new
-#      @driver.type("username","")
-
+    def test_login
         @driver.navigate.to('https://test.salesforce.com/')
-	element = @driver.find_element(:name, 'username')
-	element.send_keys "dvanderwatt@vepcg.com.edo0"
-	element = @driver.find_element(:name, 'pw')
-	element.send_keys "test2014"
-	element.submit
+      	element = @driver.find_element(:name, 'username')
+      	element.send_keys "dvanderwatt@vepcg.com.edo0"
+      	element = @driver.find_element(:name, 'pw')
+      	element.send_keys "test2014"
+      	element.submit
+        # now go to opportunity
+        @driver.navigate.to('https://cs16.salesforce.com/006f0000004PyW6')
+        # then click on new quote
+        @driver.navigate.to('https://cs16.salesforce.com/servlet/servlet.Integration?scontrolCaching=1&lid=00bf0000000DtHy&eid=006f0000004PyW6&ic=1')
+        # enter into new quote
+        element.submit
+        # find the first input box and enter a quantity of 10
+        element = @driver.find_element(:name, 'j_id0:j_id7:j_id50:j_id54:0:j_id61')
+        element.send_keys "10"
 
-#      @driver.type("password","test2014")
-#      @driver.click("Login")
     end
 
   end
